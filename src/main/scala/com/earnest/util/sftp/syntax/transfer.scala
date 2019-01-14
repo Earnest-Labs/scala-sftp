b7ac66b9b3b671aa89f060a27f67501d8415e342
Onur Ozduygu <onur.zdg@gmail.com>
2019-01-14 12:15:07 -0800

    it was a bad idea to put this method here; can exhaust all connections

--- src/main/scala/com/earnest/util/sftp/syntax/transfer.scala
+++ src/main/scala/com/earnest/util/sftp/syntax/transfer.scala
@@ -3,21 +3,14 @@ package com.earnest.util.sftp.syntax
 import java.io.InputStream
 
 import com.jcraft.jsch.ChannelSftp
-import com.earnest.util.sftp.syntax.query._
 import cats.effect.Effect
 import cats.syntax.flatMap._
-import cats.syntax.traverse._
-import cats.instances.list._
 import com.earnest.util.sftp.{LeasedInputStream, SFTPDataSource}
 import scala.language.implicitConversions
 
 final class TransferOps[F[_]](val sds: SFTPDataSource[F]) extends AnyVal {
   def openStreamToFile(location: String)(implicit F: Effect[F]): F[LeasedInputStream] =
     sds.eval(F.delay(sds.acquire) >>= (lease => F.delay(LeasedInputStream(lease.get().channel.get(location), () => lease.release()))))
-
-  def openStreamsToFilesInDir(dir: String, predicate: String => Boolean)(implicit F: Effect[F]): F[List[LeasedInputStream]] =
-    sds.lsFiles(dir) >>= (xs => xs.filter(e => predicate(e.getFilename))
-      .traverse(e => openStreamToFile(dir + "/" + e.getFilename)))
 
   def upload(location: String, is: InputStream)(implicit F: Effect[F]): F[Unit] =
     sds.eval(F.delay(sds.acquire(sess => sess.channel.put(is, location, ChannelSftp.OVERWRITE))))
